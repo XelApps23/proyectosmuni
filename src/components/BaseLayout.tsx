12cba82608bb6259ea8e6995c463c82c589ef8fd
tatobig <tatobig@gmail.com>
2023-09-07 01:21:51 -0600

    Added redux store

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -1,19 +1,54 @@
+import { onAuthStateChanged } from "firebase/auth";
 import Sidebar from "./Sidebar";
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { RiSearch2Line } from "react-icons/ri";
+import { useRouter } from "next/router";
+import { useDispatch, useSelector } from 'react-redux'
+import { auth } from "@/services/Firebase";
+import { loginRedux } from "@/store/login";
 
 interface Props {
   children: ReactNode | ReactNode[];
 }
 
 export default function BaseLayout({ children }: Props) {
+  const router = useRouter()
+  const dispatch = useDispatch()
+
+  const {
+    logged
+  } = useSelector((state) => state.login)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      console.log(user);
+      if (user === null) {
+        dispatch(
+          loginRedux({
+            status: false,
+            username: undefined,
+            id: undefined,
+          })
+        );
+        router.push("/login");
+      } else {
+        dispatch(
+          loginRedux({
+            status: true,
+            username: user.email,
+            id: user.uid,
+          })
+        );
+      }
+    });
+  }, []);
+
   return (
     <div>
       <div>
         <div className="w-[200px] h-[10px] left-[800px] top-[15px] p-1 absolute bg-white rounded-lg justify-center items-center inline-flex">
           <div className="w-96 h-5 pl-5 pr-6 py-3 bg-white rounded-lg border border-slate-300 justify-start items-center gap-40 inline-flex">
-            <div
-              className="text-stone-400 text-xs font-normal p-6 " >
+            <div className="text-stone-400 text-xs font-normal p-6 ">
               Buscar.proyecto
             </div>
             <div className="p-9 justify-start items-center gap-2.5 flex ml-15">
@@ -23,8 +58,7 @@ export default function BaseLayout({ children }: Props) {
           </div>
         </div>
         <form>
-          <div className="w-full relative flex justify-center items-center p-0 left-[150px] ">
-          </div>
+          <div className="w-full relative flex justify-center items-center p-0 left-[150px] "></div>
         </form>
       </div>
       <div>
