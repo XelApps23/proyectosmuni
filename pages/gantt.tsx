86ede30356a2f9b7bc15f7e90549515b9e1a1a48
rosariogs <rosario.1999gomez@gmail.com>
2023-09-20 12:58:28 -0600

    TASK 35
    Creación de pagina inicial

---
+++ pages/gantt.tsx
@@ -0,0 +1,15 @@
+import BaseLayout from '@/src/components/BaseLayout'
+import Home from './examples'
+import Tabs from './index'
+
+const Gantt = () => {
+    return (
+
+        <div>
+            <Tabs></Tabs>
+        </div>
+    )
+}
+
+
+export default Gantt
