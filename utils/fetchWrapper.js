4f97da612cd12d9efcb241fc2d1198e3adf9b056
Wellington Cua <94945182+w4ll-3@users.noreply.github.com>
2023-10-25 11:20:25 -0600

    Corrections gantt chart
    conversion of gantt chart to componet, reading of real project tasks, responsiveness of the table to adapt to the size of the container, and rendering and desing corrections, function to convert date from timestamp to date with 'yyyy-mm-dd' for gantt chart

--- utils/fetchWrapper.js
+++ utils/fetchWrapper.js
@@ -1,23 +1,45 @@
-export function client (endpoint, { body, ...customConfig } = {}) {
-  const headers = { 'Content-Type': 'application/json' }
-  const config = {
-    method: body ? 'POST' : 'GET',
-    ...customConfig,
-    headers: {
-      ...headers,
-      ...customConfig.headers
-    }
-  }
-  if (body) {
-    config.body = JSON.stringify(body)
-  }
+import { db } from '@/services/Firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { convertDate } from './convertDataFirebase'
 
-  return window.fetch(`${endpoint}`, config).then(async (response) => {
-    if (response.ok) {
-      return await response.json()
-    } else {
-      const errorMessage = await response.text()
-      return Promise.reject(new Error(errorMessage))
+const table = 'tasks'
+
+export default async function client () {
+  try {
+    const tasks = []
+    const taskDurations = []
+
+    const querySnapshot = await getDocs(collection(db, table))
+    querySnapshot.forEach((doc) => {
+      const datos = doc.data()
+      datos.initialDate = convertDate(datos.initialDate)
+      datos.endDate = convertDate(datos.endDate)
+
+      // Agregar datos de la tarea
+      tasks.push({
+        id: doc.id,
+        name: datos.description
+      })
+
+      // Agregar datos de la duración de la tarea
+      taskDurations.push({
+        id: doc.id,
+        start: datos.initialDate,
+        end: datos.endDate,
+        task: doc.id
+      })
+    })
+
+    // Crear el objeto JSON final
+    const jsonResponse = {
+      tasks,
+      taskDurations
     }
-  })
+
+    // Convertir el objeto a JSON y mostrarlo en la consola
+    return JSON.stringify(jsonResponse, null, 2)
+  } catch (error) {
+    console.error('Error al obtener los documentos:', error)
+    return JSON.stringify({ tasks: [], taskDurations: [] })
+  }
 }
